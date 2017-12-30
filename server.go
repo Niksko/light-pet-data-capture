a10f82ae9bdbbdc9ba0305b6dee09c9c337002ff
Nikolas M Skoufis <n.skoufis@gmail.com>
2017-12-30 19:14:41 +1100

    Add basic echo server that accepts POST requests
    This adds a basic web server that accepts POST requests on the root, and returns
    method not allowed to anything else.
    
    Configured to send some headers for security, and to use the three
    cipher suites that overlap with the axTLS implementation of TLS used by
    the arduino.

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"crypto/tls"
+	"net/http"
+	"log"
+)
+
+func RootHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload")
+	response.Header().Add("X-Frame-Options", "DENY")
+	response.Header().Add("X-Content-Type-Options", "nosniff")
+
+	if (request.Method == http.MethodPost) {
+		response.WriteHeader(http.StatusOK)
+	} else {
+		response.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+
+func main() {
+    mux := http.NewServeMux()
+
+    mux.HandleFunc("/", RootHandler)
+
+    cfg := &tls.Config{
+        MinVersion:               tls.VersionTLS12,
+        CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+        PreferServerCipherSuites: true,
+        CipherSuites: []uint16{
+        	tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+        	tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+        	tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
+        },
+    }
+    srv := &http.Server{
+        Addr:         ":443",
+        Handler:      mux,
+        TLSConfig:    cfg,
+        TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+    }
+
+    log.Fatal(srv.ListenAndServeTLS("tls.crt", "tls.key"))
+}
